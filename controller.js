6d7113f23619ab80d11a79368ade293f88cc89fb
Krishnabh Medhi <kingshu.crusader@gmail.com>
2014-10-18 15:10:26 -0400

    3 motor shit

---
+++ controller.js
@@ -0,0 +1,45 @@
+var Myo = require('myo');
+var nodeNxt = require('node-nxt');
+
+nodeNxt.connect ('/dev/cu.wonderbot-DevB', function (nxt) {
+    console.log("NXT Connected");
+    
+    var myMyo = Myo.create();
+    myMyo.vibrate();
+    console.log("Myo connected");
+
+    myMyo.on('fist', function(edge){
+        console.log("Fist");  
+        nxt.OutputSetSpeed (3, 0, 0);
+        nxt.OutputSetSpeed (2, 0, 0);
+        nxt.OutputSetSpeed (1, 0, 0);
+    });
+
+    myMyo.on('fingers_spread', function(edge){
+        console.log("Spread");
+        nxt.OutputSetSpeed (3, 32, 200);
+        nxt.OutputSetSpeed (2, 32, 200);
+        nxt.OutputSetSpeed (1, 32, 200);
+    });
+
+
+    myMyo.on('wave_out', function(edge){
+        console.log("Out");
+        nxt.OutputSetSpeed (3, 32, 100);
+        nxt.OutputSetSpeed (1, 32, -100);       
+    });
+
+    myMyo.on('wave_in', function(edge){
+        console.log("Out");
+        nxt.OutputSetSpeed (3, 32, -100);
+        nxt.OutputSetSpeed (1, 32, 100);
+    });
+
+    myMyo.on('thumb_to_pinky', function(edge){
+        console.log("Out");
+        nxt.OutputSetSpeed (3, 32, -200);
+        nxt.OutputSetSpeed (2, 32, -200);
+        nxt.OutputSetSpeed (1, 32, -200);
+    });
+    
+});
