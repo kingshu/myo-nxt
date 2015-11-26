8a3c6027c4f51f315e1cdc842fca710bdf0172d6
Merctron <smartomot@hotmail.com>
2015-11-26 00:12:09 -0500

    Myo package change

--- myocontrol.js
+++ myocontrol.js
@@ -3,43 +3,57 @@ var nodeNxt = require('node-nxt');
 
 nodeNxt.connect ('/dev/tty.wonderbot-DevB', function (nxt) {
     console.log("NXT Connected");
-    
-    var myMyo = Myo.create();
-    myMyo.vibrate();
-    console.log("Myo connected");
 
-    myMyo.on('fist', function(edge){
-        console.log("Fist");  
-        nxt.OutputSetSpeed (3, 0, 0);
-        nxt.OutputSetSpeed (2, 0, 0);
-        nxt.OutputSetSpeed (1, 0, 0);
+
+    Myo.connect();
+    Myo.onError = function () {  
+            console.log("Woah, couldn't connect to Myo Connect");
+    }
+
+    var myMyo;
+
+    Myo.on('connected', function(){  
+        myMyo = this;
+        addEvents(myMyo);
+        console.log("Myo Connected!");
+        myMyo.vibrate();
+        myMyo.unlock();
     });
 
-    myMyo.on('fingers_spread', function(edge){
-        console.log("Spread");
-        nxt.OutputSetSpeed (3, 32, 400);
-        nxt.OutputSetSpeed (2, 32, -400);
-        nxt.OutputSetSpeed (1, 32, 400);
-    });
+    var addEvents = function(myo) {
+        myo.on('fist', function(edge){
+            console.log("Fist");  
+            nxt.OutputSetSpeed (3, 0, 0);
+            nxt.OutputSetSpeed (2, 0, 0);
+            nxt.OutputSetSpeed (1, 0, 0);
+        });
+
+        myo.on('fingers_spread', function(edge){
+            console.log("Spread");
+            nxt.OutputSetSpeed (3, 32, 400);
+            nxt.OutputSetSpeed (2, 32, -400);
+            nxt.OutputSetSpeed (1, 32, 400);
+        });
 
 
-    myMyo.on('wave_out', function(edge){
-        console.log("Out");
-        nxt.OutputSetSpeed (3, 32, 100);
-        nxt.OutputSetSpeed (1, 32, -100);       
-    });
+        myo.on('wave_out', function(edge){
+            console.log("Out");
+            nxt.OutputSetSpeed (3, 32, 100);
+            nxt.OutputSetSpeed (1, 32, -100);       
+        });
 
-    myMyo.on('wave_in', function(edge){
-        console.log("Out");
-        nxt.OutputSetSpeed (3, 32, -100);
-        nxt.OutputSetSpeed (1, 32, 100);
-    });
+        myo.on('wave_in', function(edge){
+            console.log("Out");
+            nxt.OutputSetSpeed (3, 32, -100);
+            nxt.OutputSetSpeed (1, 32, 100);
+        });
 
-    myMyo.on('thumb_to_pinky', function(edge){
-        console.log("Out");
-        nxt.OutputSetSpeed (3, 32, -200);
-        nxt.OutputSetSpeed (2, 32, 200);
-        nxt.OutputSetSpeed (1, 32, -200);
-    });
+        myo.on('thumb_to_pinky', function(edge){
+            console.log("Out");
+            nxt.OutputSetSpeed (3, 32, -200);
+            nxt.OutputSetSpeed (2, 32, 200);
+            nxt.OutputSetSpeed (1, 32, -200);
+        });
+    }
     
 });
